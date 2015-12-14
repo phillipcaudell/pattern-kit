c259b2a89aabdbf8ec16900acce78d511bf8fc46
Phillip Caudell <phill@3sidedcube.com>
2015-12-14 08:40:27 +0000

    Adds convenience class with PKTableViewController

---
+++ PatternKit/PKTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PKTableViewController.h
+//  PatternKit
+//
+//  Created by Phillip Caudell on 14/12/2015.
+//  Copyright © 2015 3 SIDED CUBE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKTableViewController : UITableViewController
+
+@end
