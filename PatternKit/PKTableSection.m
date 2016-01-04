5e724eb2030ac4f71f7fb98950f1d5431aa4548a
Phillip <phill@3sidedcube.com>
2016-01-04 13:20:43 +0000

    Adds sortedTableSectionsWithItems method

--- PatternKit/PKTableSection.m
+++ PatternKit/PKTableSection.m
@@ -31,4 +31,51 @@
     return self.footerTitle;
 }
 
++ (NSArray *)sortedTableSectionsWithItems:(NSArray *)items keyPath:(NSString *)keyPath ascending:(BOOL)ascending interactionHandler:(PKInteractionHandler)interactionHandler
+{
+    static NSString *miscLetter = @"#";
+    NSMutableDictionary *sortedDictionary = [NSMutableDictionary dictionary];
+    NSCharacterSet *alphabetCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+    // Breakout names into sorted dictionary
+    [items enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+       
+        NSString *name = [obj valueForKeyPath:keyPath];
+        NSString *firstLetter = [[name substringToIndex:1] uppercaseString];
+        
+        // If it's not from the alphabet, sort into "other" category
+        NSRange rangeOfAlphabetCharacter = [firstLetter rangeOfCharacterFromSet:alphabetCharacterSet];
+        if (rangeOfAlphabetCharacter.location == NSNotFound) {
+            firstLetter = miscLetter;
+        }
+        
+        NSMutableArray *letters = sortedDictionary[firstLetter];
+        
+        if (!letters) {
+            letters = [NSMutableArray array];
+            sortedDictionary[firstLetter] = letters;
+        }
+        
+        [letters addObject:obj];
+    }];
+    
+    NSMutableArray *sections = [NSMutableArray array];
+    
+    // Make sure we're alphabetical
+    NSMutableArray *keys = [[sortedDictionary.allKeys sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:nil ascending:ascending]]] mutableCopy];
+    
+    // Make sure misc items such as numbers and other characters appear last
+    [keys removeObject:miscLetter];
+    [keys addObject:miscLetter];
+    
+    // Create section for each letter
+    [keys enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL * _Nonnull stop) {
+       
+        PKTableSection *section = [PKTableSection tableSectionWithHeaderTitle:key items:sortedDictionary[key] footerTitle:nil interactionHandler:interactionHandler];
+        [sections addObject:section];
+    }];
+    
+    return sections;
+}
+
 @end
